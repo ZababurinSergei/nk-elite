61af90cf0d194d1e1ccc1cbc3b1d277461ad08c2
Zababurin <zababurinsv@vk.com>
2024-09-18 17:08:16 +0300

    update

---
+++ examples/src/component/nk-chat/index.mjs
@@ -0,0 +1,51 @@
+import { Component } from '../index.mjs';
+import { nkP2p } from './this/index.mjs'
+
+const name = 'nk-chat';
+const component = await Component();
+
+Object.defineProperties(component.prototype, {
+    stream: {
+        value: async function(stream) {
+            console.log('--------------', stream)
+        },
+        writable: true
+    },
+    connected: {
+        value: async function(property) {
+            this.broadcastChannel = {
+                await: ['nk-p2p']
+            }
+
+            this.task = {
+                id: 'nk-p2p_0',
+                component: 'nk-p2p',
+                type: 'self',
+                callback: nkP2p,
+                data: {test: 'test'}
+            }
+
+            return true;
+        },
+        writable: true
+    },
+    disconnected: async function() {
+        return true
+    },
+    onMessage: {
+        value: async function(event) {
+            console.warn('Этот метод не надо использовать. Надо сделать в вызывающем компоненте тип self')
+            debugger
+        },
+        writable: false
+    }
+});
+
+try {
+    customElements.define(name, component);
+} catch (e) {
+    console.error('error', e);
+}
+
+export default {};
+
---
+++ examples/src/component/nk-chat/this/index.mjs
@@ -0,0 +1,8 @@
+import html from './html/index.mjs'
+export { nkP2p } from './external/index.mjs'
+export { html }
+export { actions } from './actions/index.mjs'
+
+export default {
+    description: 'Подключение всех модулей компонента'
+}
