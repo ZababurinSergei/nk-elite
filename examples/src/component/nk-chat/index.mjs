3dadf7e3c8bb3e8e1f0192f6b1608ed4252a1d06
Zababurin <zababurinsv@vk.com>
2024-09-19 22:17:07 +0300

    add nk-chat

--- examples/src/component/nk-chat/index.mjs
+++ examples/src/component/nk-chat/index.mjs
@@ -1,5 +1,6 @@
 import {Component} from '../index.mjs';
 import {Actions} from "../nk-menu/this/index.mjs";
+import {lpStream} from 'it-length-prefixed-stream'
 
 const name = 'nk-chat';
 const component = await Component();
@@ -7,6 +8,63 @@ const component = await Component();
 Object.defineProperties(component.prototype, {
     DOM: {
         value: {},
+        writable: true
+    },
+    _count: {
+        value: 0,
+        writable: true
+    },
+    message: {
+        value: {},
+        writable: true
+    },
+    printSmbl: {
+        value: async function () {
+            let timeout = Math.round(Math.random() * 100);
+            incoming.textContent = incoming.textContent + globalThis.message[p]
+
+            this._count ++
+            if (this._count < globalThis.message.length) {
+                setTimeout(printSmbl, timeout);
+            } else {
+                this._count = 0
+                clearTimeout(timeout)
+                globalThis.message = ''
+            }
+        },
+        writable: true
+    },
+    handler: {
+        value: async function ({connection, stream}) {
+            console.log('INCOMING PEER_ID ', connection.remotePeer.toString())
+            const lp = lpStream(stream)
+
+            const res = await lp.read()
+
+            const output = new TextDecoder().decode(res.subarray())
+
+            globalThis.message = output
+
+            this.printSmbl()
+        },
+        writable: true
+    },
+    send: {
+        value: async function (ma, msg) {
+            if (typeof ma === "string") ma = multiaddr(ma);
+
+            const signal = AbortSignal.timeout(5000)
+
+            const stream = await node.libp2p.dialProtocol(ma, proto, {
+                signal
+            });
+
+            const lp = lpStream(stream)
+
+            await lp.write(new TextEncoder().encode(msg))
+
+            return msg
+        },
         writable: true
     },
     stream: {
@@ -22,6 +80,8 @@ Object.defineProperties(component.prototype, {
             }
 
             this.DOM = {
+                input: this.shadowRoot.querySelector('.input'),
+                output: this.shadowRoot.querySelector('.output'),
                 select: function (type) {
                     const root = this.shadowRoot.querySelector('.chat')
                     switch (type) {
@@ -62,7 +122,7 @@ Object.defineProperties(component.prototype, {
         writable: true
     },
     disconnected: async function () {
-        this.DOM.chat.refresh.call(this,'select').removeEventListener('click', this.actions.refresh)
+        this.DOM.chat.refresh.call(this, 'select').removeEventListener('click', this.actions.refresh)
         return true
     },
     onMessage: {
