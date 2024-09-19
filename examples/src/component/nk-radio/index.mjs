a435169b59e1d4d57fb181fa152e78f2ab9891a1
Zababurin <zababurinsv@vk.com>
2024-09-20 00:47:46 +0300

    update

--- examples/src/component/nk-radio/index.mjs
+++ examples/src/component/nk-radio/index.mjs
@@ -36,50 +36,50 @@ Object.defineProperties(component.prototype, {
     },
     onMessage: {
         value: async function(self, detail) {
-            // this.dialog.open({
-            //     type: 'update_s',
-            //     title: 'title',
-            //     mapping: ['code', 'name', 'cancel', 'save'],
-            //     detail: {
-            //         itemId: 'dataset.id'
-            //     },
-            //     input: [{
-            //         type: 'text:mss',
-            //         id: 'code',
-            //         title: 'Код',
-            //         placeholder: 'Введите код',
-            //         isReadonly: false,
-            //         notification: "Введите код",
-            //         field: "code",
-            //         value: 'текст 2',
-            //         error: {
-            //             class: "errorMessage code",
-            //             description: 'Заполните обязательное поле'
-            //         },
-            //     }, {
-            //         type: 'text:mss',
-            //         id: 'name',
-            //         title: 'Наименование',
-            //         placeholder: 'Введите наименование',
-            //         isReadonly: false,
-            //         notification: "Введите наименование",
-            //         field: "name",
-            //         value: 'текст',
-            //         error: {
-            //             class: "errorMessage name",
-            //             description: 'Заполните обязательное поле'
-            //         },
-            //     }],
-            //     button: [{
-            //         id: 'after',
-            //         type: 'cancel',
-            //         description: 'Отмена'
-            //     }, {
-            //         id: 'after',
-            //         type: 'update',
-            //         description: 'Хорошо'
-            //     }]
-            // })
+            this.dialog.open({
+                type: 'update_s',
+                title: 'title',
+                mapping: ['code', 'name', 'cancel', 'save'],
+                detail: {
+                    itemId: 'dataset.id'
+                },
+                input: [{
+                    type: 'text:mss',
+                    id: 'code',
+                    title: 'Код',
+                    placeholder: 'Введите код',
+                    isReadonly: false,
+                    notification: "Введите код",
+                    field: "code",
+                    value: 'текст 2',
+                    error: {
+                        class: "errorMessage code",
+                        description: 'Заполните обязательное поле'
+                    },
+                }, {
+                    type: 'text:mss',
+                    id: 'name',
+                    title: 'Наименование',
+                    placeholder: 'Введите наименование',
+                    isReadonly: false,
+                    notification: "Введите наименование",
+                    field: "name",
+                    value: 'текст',
+                    error: {
+                        class: "errorMessage name",
+                        description: 'Заполните обязательное поле'
+                    },
+                }],
+                button: [{
+                    id: 'after',
+                    type: 'cancel',
+                    description: 'Отмена'
+                }, {
+                    id: 'after',
+                    type: 'update',
+                    description: 'Хорошо'
+                }]
+            })
             console.log('--------------', {
                 this: this,
                 self: self,
