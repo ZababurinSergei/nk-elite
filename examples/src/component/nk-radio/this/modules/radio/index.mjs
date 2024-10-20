5000c8bec5ec5337686d1500b6afd72769b3e467
Zababurin <zababurinsv@vk.com>
2024-10-20 23:19:46 +0300

    update

--- examples/src/component/nk-radio/this/modules/radio/index.mjs
+++ examples/src/component/nk-radio/this/modules/radio/index.mjs
@@ -2,10 +2,6 @@ import initFreeQueue from '@newkind/initFreeQueue'
 import { FreeQueue } from '@newkind/FreeQueue'
 import { FreeQueueSab } from '@newkind/FreeQueueSAB'
 
-// import initFreeQueue from "../../free-queue/free-queue.asm.js";
-// import FreeQueue from "../../free-queue/free-queue.js";
-// import FreeQueue from "../../free-queue/free-queue-sab.js";
-
 import {getConstants} from '@newkind/constants'
 import Application from "./oscilloscope/index.mjs";
 import {logger} from "@libp2p/logger";
